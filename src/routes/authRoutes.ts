69a8a9b6e316ac837ed115832549a9bd8168aae6
Oleg Krasno <krasno1986@gmail.com>
2026-06-01 12:55:05 +0200

    added swagger

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,37 @@ import { registerUser, loginUser } from '../controllers/authController';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Регистрация нового пользователя
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Пользователь успешно создан
+ *       400:
+ *         description: Ошибка валидации или пользователь уже существует
+ */
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import connectDB from './config/db';
 import productRoutes from './routes/productRoutes';
 import authRoutes from './routes/authRoutes';
 import categoryRoutes from "./routes/categoryRoutes";
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 dotenv.config();
 
@@ -11,6 +13,35 @@ connectDB();
 
 const app: Application = express();
 
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'E-Commerce API',
+            version: '1.0.0',
+            description: 'API документация для нашего интернет-магазина',
+        },
+        servers: [
+            {
+                url: 'http://127.0.0.1:3000',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+    },
+    // apis: ['./src/routes/*.ts'],
+    apis: ['./src/routes/*.ts', './src/routes/*.js', './dist/routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
